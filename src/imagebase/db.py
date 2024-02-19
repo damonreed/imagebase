5fde6420b1662596c90deef0db8777b506235d1f
Damon Reed <dreed@nomadia.org>
2024-02-19 09:04:26 -0600

    DB rewrite- switched to dynamic schema and table path.

--- src/imagebase/db.py
+++ src/imagebase/db.py
@@ -1,4 +1,5 @@
 from google.cloud import bigquery as bq
+from google.cloud import bigquery as bq
 
 SCHEMA = [
     bq.SchemaField("id", "STRING", mode="REQUIRED"),
@@ -23,28 +24,58 @@ class DB:
         self.client = bq.Client(project=project)
         self.path = f"{self.project}.{self.dataset}.{self.table}"
 
-    def __create_table(self):
+    def create_table(self, schema):
         # Check if the table exists, if not, create it
         try:
             table = self.client.get_table(self.path)
             print(f"Table {self.path} already exists.")
         except:
             print(f"Table {self.path} does not exist. Creating it now...")
-            table = bq.Table(self.path, schema=SCHEMA)
+            table = bq.Table(self.path, schema=schema)
             table = self.client.create_table(table)
             print(f"Table {table} created successfully.")
 
-    def __delete_table(self):
+    def delete_table(self):
         try:
             self.client.delete_table(self.path)
             print(f"Table {self.path} deleted successfully.")
         except:
             print(f"Table {self.path} does not exist.")
 
+    def get_schema(self):
+        table = self.client.get_table(self.path)
+        return table.schema
+
+    # Add a column to the table
+    def add_column(self, column_name, column_type, column_mode):
+        table = self.client.get_table(self.path)  # Make an API request.
+
+        original_schema = table.schema
+        new_schema = original_schema[:]  # Creates a copy of the schema.
+        new_schema.append(bq.SchemaField(column_name, column_type, mode=column_mode))
+
+        table.schema = new_schema
+        table = self.client.update_table(table, ["schema"])  # Make an API request.
+
+        if len(table.schema) == len(original_schema) + 1 == len(new_schema):
+            print("A new column has been added.")
+        else:
+            print("The column has not been added.")
+
+    def remove_column(self, column_name):
+        query = f"""
+            ALTER TABLE `{self.path}`
+            DROP COLUMN {column_name}
+        """
+        query_job = self.client.query(query)  # Make an API request.
+        response = query_job.result()  # Waits for query to finish
+        print(f"{response}")
+
     # Reset the table
-    def __reset_table(self):
-        self.__delete_table()
-        self.__create_table()
+    def reset_table(self):
+        schema = self.get_schema()
+        self.delete_table()
+        self.create_table(schema)
 
     ##
     ## CRUD Functions
--- test.py
+++ test.py
@@ -1,1 +1,37 @@
-from src import imagebase
+from src.imagebase import db
+from google.cloud import bigquery as bq
+from pprint import pprint as pp
+
+project = "imagebase-414802"
+dataset = "imagebase"
+table = "fantasy"
+
+SCHEMA = [
+    bq.SchemaField("id", "STRING", mode="REQUIRED"),
+    bq.SchemaField("url", "STRING", mode="REQUIRED"),
+    bq.SchemaField("blob", "BYTES", mode="NULLABLE"),
+    bq.SchemaField("title", "STRING", mode="NULLABLE"),
+    bq.SchemaField("prompt", "STRING", mode="NULLABLE"),
+    bq.SchemaField("tags", "STRING", mode="REPEATED"),
+    bq.SchemaField("model", "STRING", mode="REQUIRED"),
+]
+
+db_client = db.DB(project=project, dataset=dataset, table=table)
+db_client.create_table(SCHEMA)
+
+pp(db_client.get_schema())
+
+for field in db_client.get_schema():
+    if field.name == "new_column":
+        db_client.remove_column("new_column")
+
+pp(db_client.get_schema())
+
+db_client.add_column("new_column", "STRING", "NULLABLE")
+pp(db_client.get_schema())
+
+db_client.reset_table()
+
+pp(db_client.get_schema())
+
+db_client.delete_table()
